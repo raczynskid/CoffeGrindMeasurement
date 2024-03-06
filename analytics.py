49b1a757fb49a4d4046445625bf9b822c49d5118
raczynskid <raczynski90@gmail.com>
2024-03-06 17:18:01 +0100

    statistical functions to calculate uniformity

--- analytics.py
+++ analytics.py
@@ -5,11 +5,25 @@ def particles_to_dataframe(points: np.array, sizes: np.array) -> pd.DataFrame:
     # reformat particle coordinates and sizes into a pandas dataframe
     return pd.DataFrame({'point_x': [point[0]for point in points], "points_y": [point[1]for point in points], 'point size': sizes})
 
-def calculate_variance(point_list : pd.DataFrame) -> float:
+def calculate_zscore(sizes: np.array) -> pd.DataFrame:
+    # for each point calculate relationship to the mean
+    size_series = pd.DataFrame({'point size': sizes})
+    size_series.sort_values(by=["point size"], inplace=True)
+    size_series["zscore"] = (size_series - size_series.mean())/size_series.std()
+    return size_series
+
+def drop_quantile(sizes: np.array, quantile: float=0.8) -> pd.Series:
+    # calculate a top quantile for the data and drop it
+    # only top quantile is dropped as the outliers to remove are clumps of particles
+    size_series = pd.DataFrame({'point size': sizes})
+    quantile_threshold = size_series["point size"].quantile(0.8)
+    return size_series[size_series["point size"] < quantile_threshold]
+
+def calculate_variance(sizes: pd.DataFrame) -> float:
     # calculate Variance using the point sizes list
-    point_size = point_list["point size"]
+    size_series = sizes["point size"]
     # calculate mean of dataset
-    size_mean = point_size.mean()
+    size_mean = size_series.mean()
     # for each point of dataset ((X - mean)^2) / number of datapoints
-    return point_size.apply(lambda x: pow((x - size_mean), 2)).sum() / point_size.count()
+    return size_series.apply(lambda x: pow((x - size_mean), 2)).sum() / size_series.count()
     
